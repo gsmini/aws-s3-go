03c91e4e7d63022edaa7178841673eb0c2d44447
gsmini <gsmini@sina.cn>
2022-11-17 15:51:17 +0800

    feat 完成 查看文件列表的v2 sdk 功能

--- README.md
+++ README.md
@@ -17,6 +17,6 @@
 - gen-file-sign-url 生成带有效期的访问签名
  > https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/userguide/example_s3_Scenario_PresignedUrl_section.html
 - list-object-by-page-v1 分页查看文件列表v1版本的sdk实现
- > https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/API/API_ListObjectsV2.html
+ > https://docs.aws.amazon.com/sdk-for-go/api/service/s3/#S3.ListObjectsPages
 - list-object-by-page-v2 分页查看文件列表v2版本的sdk实现
- 
+ > https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/API/API_ListObjectsV2.html
---
+++ list-object-by-page-v2/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"time"
+)
+
+func listObjectPage(pageSize int64, startAfter string, prefix string) (data []string, count int64) {
+	start := time.Now() // 获取当前时间
+	bucket := "uniquebucket1233"
+	region := "ap-northeast-1"
+	awsAccessKeyId := "AKIA5GCI6ENEWRGCAP7D"
+	awsSecretAccessKey := "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"
+	// 1668268860000 2022-11-13 00:01:00 算到小时 按照前缀匹配
+	//today_prefix := "iot/xdasj12dxj/2022/11/13/continuous_event"
+
+	sess, _ := session.NewSession(&aws.Config{
+		Region:      aws.String(region),                                                       //桶所在的区域
+		Credentials: credentials.NewStaticCredentials(awsAccessKeyId, awsSecretAccessKey, ""), //sts的临时凭证
+	})
+
+	svc := s3.New(sess)
+
+	StartAfter := aws.String(startAfter)
+
+	svc.ListObjectsV2Pages(&s3.ListObjectsV2Input{Bucket: aws.String(bucket), Prefix: &prefix, MaxKeys: &pageSize, StartAfter: StartAfter},
+		func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+			fmt.Println(*page.KeyCount)
+			for _, item := range page.Contents {
+				fmt.Println("Name:         ", *item.Key)
+				data = append(data, *item.Key)
+
+			}
+
+			return false //翻页
+		})
+
+	elapsed := time.Since(start)
+	//go build && ./main
+	fmt.Println("开始时间：", start)
+	fmt.Println("该函数执行完成耗时：", elapsed)   //4.316053768s  2.431293798s
+	fmt.Println("两天获取的数据大小为", len(data)) //2880 之前生成了两天的数据 60*24 *2(1min上传一次视频文件)
+	return data, int64(len(data))
+}
+func main() {
+	var pageSize int64 = 500
+	var count int64 = 0
+	var result []string
+	startAfter := ""
+	prefix := "iot/xdasj12dxj/2022/11/13/continuous"
+
+	//假设这是前段访问：每页pageSize个，如果返回的结果小于pageSize，说明已经是最后一页
+	for {
+		data, dataCount := listObjectPage(pageSize, startAfter, prefix)
+		count = dataCount
+		startAfter = data[count-1] //取最后一个作为筛选过滤的s3 key
+		fmt.Printf("当前获取的数据长度为:%d", dataCount)
+		result = append(result, data...)
+		if count < pageSize {
+			break
+		}
+
+	}
+	fmt.Println(len(result))
+
+}
